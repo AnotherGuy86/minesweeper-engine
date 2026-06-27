ec1bd3964032ff4aa4fb901ae06f476004842cea
AnotherGuy <aqil.damas@gmail.com>
2026-06-27 13:37:48 +0400

    Add visibility - Update print

--- main.py
+++ main.py
@@ -39,15 +39,18 @@ class Board():
         cell= self.board[row][col]
         cell.flagged = not cell.flagged
     
-
-    def print(self):
+    def print(self,make_visible=False):
         #print the current case of the board to the terminal
         sprite_board = ""
         for row in self.board:
             for cell in row:
-                if cell.bomb: sprite_board += "X"
-                elif cell.value == 0: sprite_board += "_"
-                else: sprite_board += str(cell.value)
+                if cell.visible or make_visible:
+                    if cell.bomb: sprite_board += "X"
+                    elif cell.value == 0: sprite_board += " "
+                    else: sprite_board += str(cell.value)
+                else:
+                    if cell.flagged: sprite_board += "F"
+                    else: sprite_board += "_"
 
             sprite_board += "\n"
         
@@ -55,11 +58,12 @@ class Board():
     pass
 
 class Cell():
-    def __init__(self,row,col,bomb=0,flagged=0,value = 0):
+    def __init__(self,row,col,bomb=0,flagged=0,value = 0,visible=0):
             self.row = row
             self.col = col
             self.bomb = bomb
             self.flagged = flagged
             self.value = value #this is inclusive if its a bomb
+            self.visible = visible
     pass
 
